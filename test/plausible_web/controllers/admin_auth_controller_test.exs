32bdc98fb165cf76912443cbda1c22362d1b1317
Max Barrass <max.barrass@gmail.com>
2026-07-22 23:45:26 +1000

    Test forced SSO registration redirect

--- test/plausible_web/controllers/admin_auth_controller_test.exs
+++ test/plausible_web/controllers/admin_auth_controller_test.exs
@@ -9,6 +9,21 @@ defmodule PlausibleWeb.AdminAuthControllerTest do
       patch_config(disable_registration: true)
       conn = get(conn, "/register")
       assert redirected_to(conn) == "/login"
+      assert Phoenix.Flash.get(conn.assigns.flash, :error) ==
+               "Registration is disabled on this instance"
+    end
+
+    test "disabled registration redirects to forced Creator Signal SSO without an error flash", %{
+      conn: conn
+    } do
+      insert(:user)
+      patch_config(disable_registration: true)
+      patch_env(CreatorSignal.PlausibleSSO.Config, enabled: true, force_login: true)
+
+      conn = get(conn, "/register")
+
+      assert redirected_to(conn) == "/creator-signal/sso/login"
+      refute Phoenix.Flash.get(conn.assigns.flash, :error)
     end
 
     test "disable registration + first launch", %{conn: conn} do
